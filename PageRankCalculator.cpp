0a07e9709c0dcf98ca402d207cfba12c7d4cd9cf
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-03-20 02:16:11 +0100

    orgenize classes

--- PageRankCalculator.cpp
+++ PageRankCalculator.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "PageRankCalculator.h"
+vector<double> PageRankCalculator::calculatePageRank(Graph g) {
+    // apply the PageRank algorithm
+    // Graph g(node.size());
+    vector<double> rank_vec(num_vertices(g));
+    page_rank(g, &rank_vec[0]);
+
+    rank_vec.insert(rank_vec.begin(),all_page_rank.begin(),all_page_rank.end());
+    all_page_rank=rank_vec;
+
+    //  node.insert(node.begin(),nodes.begin(),nodes.end());
+    // nodes=node;
+    return rank_vec;
+}
+void PageRankCalculator::printPageRank() {
+    // print the ranks
+    for (long i = 0; i < all_page_rank.size(); i++) {
+        std::cout << "Node " << i << " has PageRank " << all_page_rank[i] << std::endl;
+    }
+}
+const vector<Node> & PageRankCalculator::getNodes() const {
+    return nodes;
+}
+
+void PageRankCalculator::setNodes(const vector<Node> &nodes) {
+    PageRankCalculator::nodes = nodes;
+}
+
+const vector<double> & PageRankCalculator::getAllPageRank() const {
+    return all_page_rank;
+}
+
+void PageRankCalculator::setAllPageRank(const vector<double> &allPageRank) {
+    all_page_rank = allPageRank;
+}
